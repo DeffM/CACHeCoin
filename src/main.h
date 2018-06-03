fa34a199b9e68af7ab22b392ec98613929bcdbe7
Deff <b-centr@mail.ru>
2018-06-03 22:07:19 +0700

    MemPool remove conflicts added

--- src/main.h
+++ src/main.h
@@ -1693,10 +1693,11 @@ public:
     std::map<uint256, CTransaction> mapTx;
     std::map<COutPoint, CInPoint> mapNextTx;
 
+    bool removeConflicts(const CTransaction &tx);
     bool accept(CTxDB& txdb, CTransaction &tx,
                 bool fCheckInputs, bool* pfMissingInputs);
     bool addUnchecked(const uint256& hash, CTransaction &tx);
-    bool remove(CTransaction &tx);
+    bool remove(const CTransaction &tx, bool fRecursive = false);
     void clear();
     void queryHashes(std::vector<uint256>& vtxid);
 
